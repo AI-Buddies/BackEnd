965cb843891670d20e2065ce20b8360d7a8a9021
LimSeHyeon <116863184+LimSeHyeon@users.noreply.github.com>
2025-05-20 22:22:49 +0900

    init: 프로젝트 초기 생성본 업로드

---
+++ src/main/java/com/example/sketchTalk/SketchTalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.sketchTalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SketchTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SketchTalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sketchTalk/SketchTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sketchTalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SketchTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
