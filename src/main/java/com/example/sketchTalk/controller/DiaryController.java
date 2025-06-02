ac87efdb242e8f9bc0a0a0fd00953a08471d4e69
LimSeHyeon <dlatpgus1205@naver.com>
2025-06-02 18:33:46 +0900

    feat : 일기 수정 API
    #7

--- src/main/java/com/example/sketchTalk/controller/DiaryController.java
+++ src/main/java/com/example/sketchTalk/controller/DiaryController.java
@@ -1,14 +1,13 @@
 package com.example.sketchTalk.controller;
 
+import com.example.sketchTalk.dto.diary.in.ModifyDiaryReq;
 import com.example.sketchTalk.dto.diary.in.SaveDiaryReq;
+import com.example.sketchTalk.dto.diary.out.ModifyDiaryRes;
 import com.example.sketchTalk.dto.diary.out.SaveDiaryRes;
 import com.example.sketchTalk.service.DiaryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/diary")
@@ -20,5 +19,11 @@ public class DiaryController {
     public ResponseEntity<SaveDiaryRes> saveDiary(@RequestBody SaveDiaryReq req) {
         SaveDiaryRes saveDiaryRes = diaryService.putDiary(req);
         return ResponseEntity.ok(saveDiaryRes);
+    }
+
+    @PutMapping("/")
+    public ResponseEntity<ModifyDiaryRes> modifyDiary(@RequestBody ModifyDiaryReq req) {
+        ModifyDiaryRes modifyDiaryRes = diaryService.changeDiary(req);
+        return ResponseEntity.ok(modifyDiaryRes);
     }
 }
--- src/main/java/com/example/sketchTalk/service/DiaryService.java
+++ src/main/java/com/example/sketchTalk/service/DiaryService.java
@@ -1,11 +1,19 @@
 package com.example.sketchTalk.service;
 
+import com.example.sketchTalk.dto.diary.in.ModifyDiaryReq;
 import com.example.sketchTalk.dto.diary.in.SaveDiaryReq;
+import com.example.sketchTalk.dto.diary.out.ModifyDiaryRes;
 import com.example.sketchTalk.dto.diary.out.SaveDiaryRes;
+import com.example.sketchTalk.exception.diary.DiaryException;
+import com.example.sketchTalk.model.Emotion;
 import com.example.sketchTalk.model.entity.Diary;
 import com.example.sketchTalk.repository.DiaryRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,5 +24,23 @@ public class DiaryService {
         Diary diary = new Diary(saveDiaryReq);
         Diary savedDiary = diaryRepository.save(diary);
         return new SaveDiaryRes(savedDiary.getDiaryId());
+    }
+
+    @Transactional
+    public ModifyDiaryRes changeDiary(ModifyDiaryReq modifyDiaryReq) {
+        //다이어리 찾기
+        Diary diary = diaryRepository.findById(modifyDiaryReq.diaryId()).orElseThrow(()->new DiaryException(modifyDiaryReq.diaryId()));
+        diary.rewriteDiary(modifyDiaryReq.title(), modifyDiaryReq.content(), modifyDiaryReq.emotion());
+        diaryRepository.save(diary);
+
+        ModifyDiaryRes modifyDiaryRes = ModifyDiaryRes.builder()
+                .diaryId(diary.getDiaryId())
+                .userId(diary.getUserId())
+                .title(diary.getTitle())
+                .content(diary.getContent())
+                .date(diary.getDate())
+                .emotion(diary.getEmotion())
+                .build();
+        return modifyDiaryRes;
     }
 }
