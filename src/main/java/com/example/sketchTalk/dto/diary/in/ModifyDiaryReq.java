4610a3a1f6e0a13fb90ab64303511b54f36db496
LimSeHyeon <dlatpgus1205@naver.com>
2025-06-02 14:39:48 +0900

    update : 일기 수정에 필요한 데이터 수정
    #7

--- src/main/java/com/example/sketchTalk/dto/diary/in/ModifyDiaryReq.java
+++ src/main/java/com/example/sketchTalk/dto/diary/in/ModifyDiaryReq.java
@@ -6,10 +6,8 @@ import java.util.Date;
 
 public record ModifyDiaryReq(
         Long diaryId,
-        Long userId,//추후에 User로 변경
         String title,
         String content,
-        Date date,
         Emotion emotion
 ) {
 }
