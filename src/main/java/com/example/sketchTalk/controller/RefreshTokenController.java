9548988dac6693035b0bfb0f07b5e066fc6fbac5
HeeFlower5001 <gmltmd3tp@naver.com>
2025-10-23 02:19:22 +0900

    refactor: ApiResonse 사용 방법 통일

--- src/main/java/com/example/sketchTalk/controller/RefreshTokenController.java
+++ src/main/java/com/example/sketchTalk/controller/RefreshTokenController.java
@@ -5,7 +5,6 @@ import com.example.sketchTalk.dto.refreshToken.RefreshReq;
 import com.example.sketchTalk.dto.refreshToken.RefreshRes;
 import com.example.sketchTalk.service.RefreshTokenService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,13 +19,9 @@ public class RefreshTokenController {
     }
 
     @PostMapping("/refresh")
-    public ResponseEntity<ApiResponse<RefreshRes>> reIssueAccessToken(@RequestBody RefreshReq refreshReq) {
+    public ApiResponse<RefreshRes> reIssueAccessToken(@RequestBody RefreshReq refreshReq) {
         RefreshRes result = refreshTokenService.reissueAccessToken(refreshReq);
 
-        ApiResponse<RefreshRes> body = ApiResponse.onSuccess(HttpStatus.OK, result);
-
-        return ResponseEntity
-                .status(body.getHttpStatus())
-                .body(body);
+        return ApiResponse.onSuccess(HttpStatus.OK, result);
     }
 }
