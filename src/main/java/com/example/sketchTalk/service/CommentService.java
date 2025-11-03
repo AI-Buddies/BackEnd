89a794f5824fad22028811f0c76dde159e2f79aa
yoolha <dbstjwls0247@naver.com>
2025-11-03 22:00:54 +0900

    refactor: 엔티티 참조 수정

--- src/main/java/com/example/sketchTalk/service/CommentService.java
+++ src/main/java/com/example/sketchTalk/service/CommentService.java
@@ -1,10 +1,14 @@
 package com.example.sketchTalk.service;
 
+import com.example.sketchTalk._core.error.CustomException;
 import com.example.sketchTalk.dto.comment.in.SaveCommentReq;
 import com.example.sketchTalk.dto.comment.out.ReqContentRes;
 import com.example.sketchTalk.dto.comment.out.SaveCommentRes;
+import com.example.sketchTalk.exception.diary.DiaryExceptions;
 import com.example.sketchTalk.model.entity.Comment;
+import com.example.sketchTalk.model.entity.Diary;
 import com.example.sketchTalk.repository.CommentRepository;
+import com.example.sketchTalk.repository.DiaryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,9 +16,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CommentService {
     private final CommentRepository commentRepository;
+    private final DiaryRepository diaryRepository;
 
     public SaveCommentRes putComment(SaveCommentReq saveCommentReq) {
-        Comment comment = new Comment(saveCommentReq.diaryId(), saveCommentReq.content());
+        Diary diary = diaryRepository.findById(saveCommentReq.diaryId()).orElseThrow(() -> new CustomException(DiaryExceptions.DIARY_NOT_FOUND));
+
+        Comment comment = new Comment(diary, saveCommentReq.content());
         Comment savedComment = commentRepository.save(comment);
         return new SaveCommentRes(savedComment.getCommentId(), savedComment.getContent());
     }
