2ab9196dcb867f4b27708e6541d80b36ea9e6249
yoolha <dbstjwls0247@naver.com>
2025-11-13 00:00:45 +0900

    feat: 도전과제 리스트/단일 조회 API 구현

--- src/main/java/com/example/sketchTalk/repository/achievement/SubCategoryRepository.java
+++ src/main/java/com/example/sketchTalk/repository/achievement/SubCategoryRepository.java
@@ -1,7 +1,17 @@
 package com.example.sketchTalk.repository.achievement;
 
+import com.example.sketchTalk.model.entity.achievement.Category;
 import com.example.sketchTalk.model.entity.achievement.SubCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface SubCategoryRepository extends JpaRepository<SubCategory, Integer> {
+import java.util.List;
+
+public interface SubCategoryRepository extends JpaRepository<SubCategory, Long> {
+    int countByCategory(Category category);
+
+    @Query("SELECT s.subId FROM SubCategory s WHERE s.category = :category")
+    List<Long> findIdsByCategory(Category category);
+
+    List<SubCategory> findByCategory(Category category);
 }
