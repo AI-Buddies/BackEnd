5885555767e6eb708403ff070e49d0efbcf28da8
HeeFlower5001 <gmltmd3tp@naver.com>
2025-11-17 03:11:18 +0900

    chore: FirebaseProperties 설정 추가

--- src/main/java/com/example/sketchTalk/config/firebase/FirebaseProperties.java
+++ src/main/java/com/example/sketchTalk/config/firebase/FirebaseProperties.java
@@ -1,11 +1,15 @@
 package com.example.sketchTalk.config.firebase;
 
 import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
-import org.springframework.stereotype.Component;
 
 @Getter
-@Component
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "firebase")
 public class FirebaseProperties {
     private Resource serviceAccount;
     private String projectId;
