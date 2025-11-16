ae431d236741eeb9458a2aed873cf57ef25005ea
HeeFlower5001 <gmltmd3tp@naver.com>
2025-11-17 02:53:21 +0900

    feat: 과거 일기 알림 스케줄러 구현
    - @Scheduled 기반으로 과거 작성한 일기를 확인하도록 알림을 전송하는 스케줄러 추가
    - 과거 일기 조회에 필요한 DiaryRepository 메서드 추가
    - PastNotificationSettingRepository에 시간 기반 조회 메서드 추가

--- src/main/java/com/example/sketchTalk/repository/DiaryRepository.java
+++ src/main/java/com/example/sketchTalk/repository/DiaryRepository.java
@@ -12,6 +12,6 @@ import java.util.Optional;
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
     List<GetCalanderDiraryRes> findAllByUserIdAndDateBetween(Long userId, LocalDate dateAfter, LocalDate dateBefore);
     List<Diary> findAllByUserIdAndDateBetweenOrderByDateAsc(Long userId, LocalDate start, LocalDate end);
-
+    List<Diary> findAllByUserIdAndDateIs(Long userId, LocalDate date);
     Optional<Diary> findByUserIdAndDiaryId(Long userId, Long diaryId);
 }
